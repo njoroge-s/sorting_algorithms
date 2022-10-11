3768564811a1accda6b39d145cb1d2f040b20b47
njoroge-s <sarahnjoroge2015@gmail.com>
2022-10-11 13:29:55 +0300

    fix error

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,31 +1,6 @@
 #include "sort.h"
 
 /**
- * radix_sort - sorts an array following the Radix sort algorithm
- * @array: array of ints to sort
- * @size: size of the array
- */
-void radix_sort(int *array, size_t size)
-{
-	int max;
-	size_t i, lsd;
-
-	if (!array || size < 2)
-		return;
-
-	max = 0;
-	for (i = 0; i < size; i++)
-		if (array[i] > max)
-			max = array[i];
-
-	for (lsd = 1; max / lsd > 0; lsd *= 10)
-	{
-		count_sort_LSD(array, size, lsd);
-		print_array(array, size);
-	}
-}
-
-/**
  * count_sort_LSD - count sort with LSD
  * @array: array to sort
  * @size: size of the array
@@ -53,4 +28,29 @@ void count_sort_LSD(int *array, size_t size, size_t lsd)
 		array[n] = out_arr[n];
 
 	free(out_arr);
+}
+
+/**
+ * radix_sort - sorts an array following the Radix sort algorithm
+ * @array: array of ints to sort
+ * @size: size of the array
+ */
+void radix_sort(int *array, size_t size)
+{
+	int max;
+	size_t i, lsd;
+
+	if (!array || size < 2)
+		return;
+
+	max = 0;
+	for (i = 0; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+
+	for (lsd = 1; max / lsd > 0; lsd *= 10)
+	{
+		count_sort_LSD(array, size, lsd);
+		print_array(array, size);
+	}
 }
--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,16 +1,31 @@
 #include "sort.h"
 
 /**
- * bitonic_sort - sorts an array following the Bitonic sort algorithm
- * @array: array of ints to sort
- * @size: size of the array
+ * bitonic_merge - sorts and merges a sequence in ascending or descending order
+ * @array: array to sort
+ * @l: index of the left-most element
+ * @r: index of the right-most element
+ * @direction: ascending or descending
  */
-void bitonic_sort(int *array, size_t size)
+void bitonic_merge(int *array, int l, int r, int direction)
 {
-	if (!array || size < 2)
-		return;
+	int tmp, i, step = (l + r) / 2, mid = (r - l + 1) / 2;
 
-	bitonic_recursion(array, 0, size - 1, 1, size);
+	if (r - l >= 1)
+	{
+		for (i = l; i < l + mid; i++)
+		{
+			if (direction == (array[i] > array[i + mid]))
+			{
+				tmp = array[i + mid];
+				array[i + mid] = array[i];
+				array[i] = tmp;
+			}
+		}
+		bitonic_merge(array, l, step, direction);
+		bitonic_merge(array, step + 1, r, direction);
+	}
+
 }
 
 /**
@@ -47,29 +62,14 @@ void bitonic_recursion(int *array, int l, int r, int direction, size_t size)
 }
 
 /**
- * bitonic_merge - sorts and merges a sequence in ascending or descending order
- * @array: array to sort
- * @l: index of the left-most element
- * @r: index of the right-most element
- * @direction: ascending or descending
+ * bitonic_sort - sorts an array following the Bitonic sort algorithm
+ * @array: array of ints to sort
+ * @size: size of the array
  */
-void bitonic_merge(int *array, int l, int r, int direction)
+void bitonic_sort(int *array, size_t size)
 {
-	int tmp, i, step = (l + r) / 2, mid = (r - l + 1) / 2;
+	if (!array || size < 2)
+		return;
 
-	if (r - l >= 1)
-	{
-		for (i = l; i < l + mid; i++)
-		{
-			if (direction == (array[i] > array[i + mid]))
-			{
-				tmp = array[i + mid];
-				array[i + mid] = array[i];
-				array[i] = tmp;
-			}
-		}
-		bitonic_merge(array, l, step, direction);
-		bitonic_merge(array, step + 1, r, direction);
-	}
-
+	bitonic_recursion(array, 0, size - 1, 1, size);
 }
